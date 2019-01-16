414f9874d465076116fbbb1b29bf729f5ab7086e
Zheng <zh@onion.io>
2019-01-16 18:59:31 -0500

    updated init.d script url

--- scripts/autoSetup.sh
+++ scripts/autoSetup.sh
@@ -2,7 +2,7 @@
 
 wget https://www.virtualhere.com/sites/default/files/usbserver/vhusbdmipsel -O /usr/bin/vhusbdmipsel
 chmod +x /usr/bin/vhusbdmipsel
-wget '' -O /etc/init.d/virtualhere
+wget https://raw.githubusercontent.com/OnionIoT/omega2-lidar-kit/master/scripts/virtualhere -O /etc/init.d/virtualhere
 chmod +x /etc/init.d/virtualhere
 /etc/init.d/virtualhere enable
 /etc/init.d/virtualhere start
