27a5705a240cb1b17b163e26a784b83d57e93c1f
Lazar <lazardemin@gmail.com>
2019-02-11 14:03:04 -0500

    added license code block and os specific port name options

--- src/lidar_read.py
+++ src/lidar_read.py
@@ -1,3 +1,25 @@
+# The MIT License (MIT)
+#
+# Copyright (c) 2019 Onion Corporation
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+
 import serial
 
 def checksum (frame):
@@ -72,6 +94,9 @@ def onData(x):
         if processFrame(frame):
             frame = ''
 
-with serial.Serial('/dev/tty.SLAB_USBtoUART', 230400) as ser:
+portName = '/dev/tty.SLAB_USBtoUART' # For Mac OS X
+#portName = 'COM4' # For Windows
+#portName = '/dev/ttyUSB0' # For Linux
+with serial.Serial(portName, 230400) as ser:
     while 1:
         onData(ser.read())
